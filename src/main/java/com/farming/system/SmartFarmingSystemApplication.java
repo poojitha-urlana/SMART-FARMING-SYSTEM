304e9887d171d5f8b5f29da1fb4f6d8223e42332
Poojitha <220101120194@cutm.ac.in>
2024-09-20 10:38:20 +0530

    intial add files

---
+++ src/main/java/com/farming/system/SmartFarmingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.farming.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartFarmingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartFarmingSystemApplication.class, args);
+	}
+
+}
