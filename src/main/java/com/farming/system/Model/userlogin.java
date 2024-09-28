45a19630aef746d705ee4efdde9a1529d5056602
pintu kumar <220101120113@cutm.ac.in>
2024-09-28 11:29:29 +0530

    add fields

--- src/main/java/com/farming/system/Model/userlogin.java
+++ src/main/java/com/farming/system/Model/userlogin.java
@@ -1,5 +1,18 @@
 package com.farming.system.Model;
 
 public class userlogin {
-
+  private String name;
+  private String id;
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getId() {
+	return id;
+}
+public void setId(String id) {
+	this.id = id;
+}
 }
